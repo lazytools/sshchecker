9ba572c018a10abbf8d812465bcf992102d60fc9
Sc0rpion2 <Shazebhussaini1994@gmail.com>
2020-07-09 17:10:42 -0400

    fix banner code

--- main.go
+++ main.go
@@ -8,6 +8,25 @@ import (
 	"github.com/projectdiscovery/gologger"
 )
 
+const banner = `
+
+              __         __              __            
+   __________/ /_  _____/ /_  ___  _____/ /_____  _____
+  / ___/ ___/ __ \/ ___/ __ \/ _ \/ ___/ //_/ _ \/ ___/
+ (__  |__  ) / / / /__/ / / /  __/ /__/ ,< /  __/ /    
+/____/____/_/ /_/\___/_/ /_/\___/\___/_/|_|\___/_/v1.0     
+                                                       
+
+`
+
+func showBanner() {
+	gologger.Printf("%s\n", banner)
+	gologger.Printf("\t\tCreated by Shazeb.\n\n")
+
+	gologger.Labelf("Use with caution. You are responsible for your actions\n")
+	gologger.Labelf("Developers assume no liability and are not responsible for any misuse or damage.\n\n")
+}
+
 func main() {
 	showBanner()
 	//adding scanner for reading the input from terminal
@@ -32,23 +51,4 @@ func main() {
 		//		t.Log(stderr)
 	}
 
-}
-
-const banner = `
-
-              __         __              __            
-   __________/ /_  _____/ /_  ___  _____/ /_____  _____
-  / ___/ ___/ __ \/ ___/ __ \/ _ \/ ___/ //_/ _ \/ ___/
- (__  |__  ) / / / /__/ / / /  __/ /__/ ,< /  __/ /    
-/____/____/_/ /_/\___/_/ /_/\___/\___/_/|_|\___/_/v1.0     
-                                                       
-
-`
-
-func showBanner() {
-	gologger.Printf("%s\n", banner)
-	gologger.Printf("\t\tCreated by Shazeb.\n\n")
-
-	gologger.Labelf("Use with caution. You are responsible for your actions\n")
-	gologger.Labelf("Developers assume no liability and are not responsible for any misuse or damage.\n\n")
 }
