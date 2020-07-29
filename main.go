9fecf2bf0be685a69fcd9d8134c3fe1c5ed65b5e
Shazeb01 <shazebvps@gmail.com>
2020-07-29 14:35:49 -0700

    pkg

--- main.go
+++ main.go
@@ -2,66 +2,28 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"os"
 	"sync"
+
+	"pkg/runner"
 
 	"github.com/projectdiscovery/gologger"
 	"github.com/scottkiss/gosshtool"
 )
 
-const banner = `
-              __         __              __            
-   __________/ /_  _____/ /_  ___  _____/ /_____  _____
-  / ___/ ___/ __ \/ ___/ __ \/ _ \/ ___/ //_/ _ \/ ___/
- (__  |__  ) / / / /__/ / / /  __/ /__/ ,< /  __/ /    
-/____/____/_/ /_/\___/_/ /_/\___/\___/_/|_|\___/_/v1.0     
-`
-const Version = `1.0`
-
-func showBanner() {
-	gologger.Printf("%s\n", banner)
-	gologger.Printf("\t\tCreated by lazytools.\n\n")
-
-	gologger.Labelf("Use with caution. You are responsible for your actions\n")
-	gologger.Labelf("Developers assume no liability and are not responsible for any misuse or damage.\n\n")
-}
-
 var (
-	userList     string
-	passwordList string
-	//concurrency   int
-	ShowVer       bool
 	userlistSlice []string
 	passwordSlice []string
 	ipStatus      map[string]bool
-	Verbose       bool
 )
-
-func ParseOptions() {
-
-	flag.StringVar(&userList, "U", "", "Text file containing list of usernames to use")
-	flag.StringVar(&passwordList, "P", "", "Text file containing list of passwords to use")
-	flag.BoolVar(&ShowVer, "version", false, "Show current program version")
-	//flag.IntVar(&concurrency, "c", 10, "set the concurrency level")
-	flag.BoolVar(&Verbose, "v", false, "Show Verbose output.")
-	flag.Parse()
-
-}
 
 func main() {
 	userlistSlice := make([]string, 0)
 	passwordSlice := make([]string, 0)
-	ParseOptions()
-	showBanner()
-	if ShowVer {
-		gologger.Infof("Current Version: %s\n", Version)
-		os.Exit(0)
-	}
 
 	var wg sync.WaitGroup
-
+	options := runner.ParseOptions()
 	//Reading username and password list.
 	userlistSlice = reader(userList)
 	passwordSlice = reader(passwordList)
---
+++ pkg/runner/options.go
@@ -0,0 +1,32 @@
+package runner
+
+import (
+	"flag"
+	"os"
+
+	"github.com/projectdiscovery/gologger"
+)
+
+type Options struct {
+	Verbose      bool
+	UserList     string
+	PasswordList string
+	ShowVer      bool
+}
+
+func ParseOptions() *Options {
+	options := &Options{}
+
+	flag.StringVar(&options.UserList, "U", "", "Text file containing list of usernames to use")
+	flag.StringVar(&options.PasswordList, "P", "", "Text file containing list of passwords to use")
+	flag.BoolVar(&options.ShowVer, "version", false, "Show current program version")
+	flag.BoolVar(&options.Verbose, "v", false, "Show Verbose output.")
+	flag.Parse()
+
+	showBanner()
+
+	if options.ShowVer {
+		gologger.Infof("Current Version: %s\n", Version)
+		os.Exit(0)
+	}
+}
